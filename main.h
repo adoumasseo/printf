96c1cc2e002e4602e74d977d263a60f7a73c4432
Gathoni-Njogu <milky.mercy26@gmail.com>
2023-11-14 19:26:20 +0300

    _Putchar prototype inserted

--- main.h
+++ main.h
@@ -37,9 +37,10 @@ int print_hex_extra(unsigned long int num);
 int print_point(va_list val);
 int print_revs(va_list args);
 int print_rot13(va_list args);
+int _putchar(char c);
+
 
 int printf_char(va_list args);
 int printf_string(va_list args);
-int handler(char c, va_list args);
 
 #endif
