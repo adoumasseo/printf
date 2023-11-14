e73843e9d52376db1dcf252bc80a068e5fbb4073
Gathoni-Njogu <milky.mercy26@gmail.com>
2023-11-14 03:11:53 +0300

    Functions for Task 3 & 4

--- main.h
+++ main.h
@@ -25,6 +25,11 @@ int _strlenc(const char *str);
 int print_37(void);
 int print_dec(va_list args);
 int print_int(va_list args);
+int print_bin(va_list val);
+int print_unsigned(va_list args);
+int print_oct(va_list val);
+int print_hex(va_list val);
+int print_HEX(va_list val);
 
 
 int printf_char(va_list args);
